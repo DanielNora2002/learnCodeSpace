9c6ec4139519740a55dbd75e6aeb831cc94b545b
Daniel Nora <danielnora1212@gmail.com>
2025-10-31 18:17:30 +0000

    Update personal information and images in components

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Daniel Nora",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "DanielNora1212@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "microsoft",
 };
-
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
 
